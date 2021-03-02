0ef704575980dfb5f798330f6bf4aac252bfaca6
HMC <hmc@MacBook-Pro.local>
2021-03-02 21:40:43 +0900

    Problem 155

--- LEETCODE_PROBLEM_101-200/LEET_CODE_PROBLEM_155.py
+++ LEETCODE_PROBLEM_101-200/LEET_CODE_PROBLEM_155.py
@@ -27,6 +27,17 @@ Constraints:
 
     Methods pop, top and getMin operations will always be called on non-empty stacks.
 
+Runtime: 48 ms, faster than 89.33% of Python online submissions for Min Stack.
+Memory Usage: 17.5 MB, less than 64.88% of Python online submissions for Min Stack.
+
+Runtime: 52 ms, faster than 73.90% of Python online submissions for Min Stack.
+Memory Usage: 17.4 MB, less than 87.07% of Python online submissions for Min Stack.
+
+96.84 % time complexity, 44ms
+64.88 % space complexity
+
+Avg eval : 86.69 % time complexity
+           72.28 % space complexity
 """
 
 
@@ -36,31 +47,54 @@ class MinStack(object):
         """
         initialize your data structure here.
         """
+        self.min_idx = 0
+
+        self.data = []
 
     def push(self, x):
         """
         :type x: int
         :rtype: None
         """
+        if len(self.data) > 0:
+            if self.data[self.min_idx] > x:
+                self.min_idx = len(self.data)
+        self.data += [x]
 
+    '''"push", "getMin", "pop", "getMin"'''
     def pop(self):
         """
         :rtype: None
         """
+        top_val = self.data[-1]
+
+        if top_val == self.data[self.min_idx]:
+            if len(self.data) == 2:
+                self.min_idx = 0
+            elif len(self.data) > 2:
+                new_idx = len(self.data) - 2
+                for i in range(len(self.data)-1):
+                    if self.data[i] < self.data[new_idx]:
+                        new_idx = i
+                self.min_idx = new_idx
+            else:
+                pass
+
+        del self.data[-1]
+
+        return top_val
 
     def top(self):
         """
         :rtype: int
         """
+        return self.data[-1]
 
     def getMin(self):
         """
+        -> in constant time.
         :rtype: int
         """
+        return self.data[self.min_idx]
 
-# Your MinStack object will be instantiated and called as such:
-# obj = MinStack()
-# obj.push(x)
-# obj.pop()
-# param_3 = obj.top()
-# param_4 = obj.getMin()
+
